c302d3eb7497d1c1780d717e99f8e28eb7cbd5c8
Autumn-Ambrosia-Official <autumnsambrosiaofficial@gmail.com>
2025-09-28 21:37:03 +0800

    Update redirect_html.py

--- redirect_html.py
+++ redirect_html.py
@@ -1,13 +1,20 @@
 from flask import Flask, redirect
-import random
 
 app = Flask(__name__)
 
+i = 0
+
 @app.route('/')
 def home():
+  global i
   try:
     html_link = ["https://index-autumn.onrender.com", "https://autumn-ambrosia.pages.dev"]
-    return redirect(random.choice(html_link))
+    index = len(html_link)
+    i += 1
+    if i == index:
+      i = 0
+
+    return redirect(html_link[i])
   except:
     return "<h2>Failed to redirect. Please try again</h2>"
 
