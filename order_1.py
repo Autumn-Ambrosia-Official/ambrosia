62fddae28f5e6438256e9ff27647465a46e97269
Autumn-Ambrosia-Official <autumnsambrosiaofficial@gmail.com>
2025-10-05 14:43:12 +0800

    Update order_1.py

--- order_1.py
+++ order_1.py
@@ -19,7 +19,6 @@ from flask_wtf.csrf import generate_csrf
 from upstash_redis import Redis
 
 
-
 r = Redis(url="https://wealthy-filly-14272.upstash.io", token="ATfAAAIncDJmNGNiYWE4NDAxMGY0NDYyOTA1OWFjMTM3NWIwNzdiMXAyMTQyNzI")
 
 
@@ -307,10 +306,6 @@ def confirm():
                              sheet_customer_cash.append_row([sanitize_for_sheet(orderdata["customer"]),sanitize_for_sheet(order_summary),sanitize_for_sheet(email),sanitize_for_sheet(userclass),sanitize_for_sheet(phone_num),total])
                            else:
                              sheet_customer_cash2.append_row([sanitize_for_sheet(orderdata["customer"]),sanitize_for_sheet(order_summary),sanitize_for_sheet(email),sanitize_for_sheet(userclass),sanitize_for_sheet(phone_num),total])
-
-                           email_data = {"order": orderdata["order"], "email": email}
-                           response = req.post("https://script.google.com/macros/s/AKfycbyOC_-Kn-DxY3FSKrQBMqX_qikVrxz0MwXpEL_KqQnw8-IUPHeAqCWXwnULbbIrILan/exec", json=email_data, headers={'Content-Type':'application/json'})
-                           print(response.status_code)
                            r.delete(token)
                         except Exception as e:
                             return f"Error in confirm: {str(e)}" 
@@ -321,10 +316,6 @@ def confirm():
                                   sheet_customer_tng.append_row([sanitize_for_sheet(orderdata["customer"]),sanitize_for_sheet(order_summary),sanitize_for_sheet(email),sanitize_for_sheet(userclass),sanitize_for_sheet(phone_num), sanitize_for_sheet(transaction_name), total])
                              else: 
                                    sheet_customer_tng2.append_row([sanitize_for_sheet(orderdata["customer"]),sanitize_for_sheet(order_summary),sanitize_for_sheet(email),sanitize_for_sheet(userclass),sanitize_for_sheet(phone_num), sanitize_for_sheet(transaction_name), total])
-                                     
-                             email_data = {"order": orderdata["order"], "email": email}
-                             response = req.post("https://script.google.com/macros/s/AKfycbyOC_-Kn-DxY3FSKrQBMqX_qikVrxz0MwXpEL_KqQnw8-IUPHeAqCWXwnULbbIrILan/exec", json=email_data, headers={'Content-Type':'application/json'})
-                             print(response.status_code)
                              r.delete(token)
                             except Exception as e:
                               return f"Error in confirm: {str(e)}"
@@ -355,12 +346,7 @@ def reload():
    return "Failed"
  
 if __name__ == '__main__':
-    app.run(debug=False, host="0.0.0.0")
-
-
-
-
-                
+    app.run(debug=False, host="0.0.0.0")     
                         
 
 
